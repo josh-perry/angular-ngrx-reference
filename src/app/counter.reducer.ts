02b2c37f81f9219637d9b532a5dac52cdde4ff5e
Josh Perry <joshperry@outlook.com>
2021-11-07 15:52:37 +0000

    Store holds an object instead of just a number

--- src/app/counter.reducer.ts
+++ src/app/counter.reducer.ts
@@ -1,11 +1,21 @@
 import { createReducer, on } from '@ngrx/store';
 import { increment, decrement, reset } from './counter.actions';
 
-export const initialState = 0;
+export interface CounterState {
+  value: number;
+}
+
+export const initialState = {
+  value: 0
+}
+
+const newState = (state: CounterState, newData: CounterState) => {
+  return Object.assign({}, state, newData);
+}
 
 export const counterReducer = createReducer(
   initialState,
-  on(increment, (state) => state + 1),
-  on(decrement, (state) => state - 1),
-  on(reset, (state) => 0)
+  on(increment, (state) => newState(state, { value: state.value + 1 })),
+  on(decrement, (state) => newState(state, { value: state.value - 1 })),
+  on(reset, (state) => newState(state, { value: 0 }))
 );
--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 import { increment, decrement, reset } from '../counter.actions';
+import { CounterState } from '../counter.reducer';
 
 @Component({
   selector: 'app-my-counter',
@@ -11,10 +12,10 @@ import { increment, decrement, reset } from '../counter.actions';
 })
 
 export class MyCounterComponent {
-  count$: Observable<number>;
+  count$?: Observable<CounterState>;
 
-  constructor(private store: Store<{ count: number }>) {
-    this.count$ = store.select("count");
+  constructor(private store: Store<{ count: CounterState }>) {
+    this.count$ = this.store.select("count");
   }
 
   increment() {
